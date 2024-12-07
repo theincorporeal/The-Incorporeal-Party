8e4215f7d04e55670ebe45d58a57298ee052d079
theincorporeal <demarchisdominic@gmail.com>
2024-12-07 10:47:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+  alert(`Thank you, ${name}! Your message has been received.`);
+  this.reset();
+});
